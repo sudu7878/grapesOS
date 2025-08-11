eb270134194c2ed3b2401ca154dc69d0b0b63c17
sudu <sudarshanmohanchavan493412@gmail.com>
2025-08-11 17:37:30 +0530

    Changed Miscellaneous stuff

--- src/bgudakrnl.c
+++ src/bgudakrnl.c
@@ -15,44 +15,16 @@
 #include "peripherals/irq_hedr.h"
 
 
-//u32 get_el();   //gets exception level
+uint64_t get_el(void);
 
-/*void putc(void *p, char c) {
-    if (c == '\n') {
-        uart_send('\r');
-    }
-    uart_send(c);
-}*/
 
-/*void kernel_main() {
-    uart_init();
-    init_printf(0, putc);
-    
-    uart_send_string("UART online!\n");
-    gpio_pin_set_func(17, GFOutput);
-
-    while(2){               //this thing signifies that the system booted and has initialized UART.
-        gpio_pin_enable(17);
-        delay(500);
-    }
-    printf("Hello, from Baguda Kernel, grapesOS! This system is now running in 64 bit mode.");
-
-    irq_init_vectors();
-    enable_interrupt_controller();
-    irq_enable();
-
-    printf("\nException Level: %d\n", get_el());
-
-    while(1){
-        char c = uart_recv();  //receive a character from UART
-        uart_send(c);          //echo the character back
-    }  //the infinite loop to keep the kernel running
-    
-}
-*/
 void kernel_main() {
     uart_init();
-    uart_send_string("Hello world! \n");
+    uart_send_string("Hello from grapesOS!\n");
+    uart_send_string("System is now running in 64 bit mode.\n");
+    uart_send_string("Current EL: ");
+    uart_send(get_el() + '0');  //convert the exception level to char and send it
+    uart_send_string("\n");
 
     while(1){
         uart_send(uart_recv());
--- src/printf.c
+++ src/printf.c
@@ -1,3 +1,4 @@
+
 //  src/printf.c
 
 
